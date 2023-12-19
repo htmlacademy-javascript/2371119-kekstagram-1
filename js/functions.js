28888ce89466584f182515dfbd3545986f333a39
LevLvov0820 <levlvov0820@gmail.com>
2023-12-19 11:35:13 +0300

    Исправления

--- js/functions.js
+++ js/functions.js
@@ -1,58 +1,50 @@
 function isPalindrome(string) {
-  string = (string || '').toLowerCase().replace(/\W/g, '');
-  const stringArray = [...string];
-  const newArray = [];
-  stringArray.forEach((index) => {
-    newArray.unshift(index);
-  });
-  const reversedString = newArray.join('');
-  return string === reversedString;
-}
-
-function numberFromString(string) {
-  string = (string || '');
-  const numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const stringArray = [...string];
-  const newArray = [];
-  stringArray.forEach((element) => {
-    if (numbersArray.includes(element)) {
-      newArray.push(element);
-    }
-  });
-  return parseInt(newArray.join(''), 10);
-}
-
-function minLength(initial, target, additional) {
-  initial = (initial || '');
-  additional = (additional || '');
-  if (initial.length >= target) {
-    return initial;
+  if (typeof string !== 'string') {
+    return 'Не строка';
   }
-  let current = initial;
-  if (additional.length > target) {
-    current = additional.slice(0, target - current.length) + current;
-    return current;
-  }
-  while (target > current.length) {
-    if (target - current.length >= additional.length) {
-      current = additional + current;
-    } else {
-      current = additional[0] + current;
-    }
-  }
-  return current;
-}
-
-function stringLength(string, maxLength) {
-  string = (string || '');
-  if (string.length <= maxLength) {
+  string = string.toLowerCase().replace(/\W/g, '');
+  if (string.split('').reverse().join('') === string) {
     return true;
   } else {
     return false;
   }
 }
 
+function numberFromString(string) {
+  if (typeof string !== 'string' && typeof string !== 'number') {
+    return NaN;
+  }
+  if (typeof string === 'number') {
+    string = string.toString();
+  }
+  let newString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!isNaN(parseInt(string[i], 10))) {
+      newString += string[i];
+    }
+  }
+  return newString;
+}
+
+function minLength(initial, target, additional) {
+  if (typeof initial !== 'string' || typeof additional !== 'string' || typeof target !== 'number') {
+    return NaN;
+  }
+  while (initial.length < target) {
+    if (additional + initial <= target) {
+      initial = additional + initial;
+    } else {
+      initial = additional.slice(0, target - initial.length) + initial;
+    }
+  }
+  return initial;
+}
+
+function isLessThanMax(string, maxLength) {
+  return string.length <= maxLength;
+}
+
 isPalindrome();
 numberFromString();
 minLength();
-stringLength();
+isLessThanMax();
