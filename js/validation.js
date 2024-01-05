b4519eed5ce18d2dc07c14721169ecd851e97042
LevLvov0820 <levlvov0820@gmail.com>
2024-01-05 11:00:36 +0300

    Правда или действие-2

---
+++ js/validation.js
@@ -0,0 +1,19 @@
+const startsWithSymbol = (string, symbol = '#') => string[0] === symbol;
+
+const hasValidLength = (string, minLength = 2, maxLength = 20) => string.length >= minLength && string.length <= maxLength;
+
+const hasValidSymbols = (string, invalidSymbols = /[^a-zA-Z0-9а-яА-ЯёЁ]/g) => !invalidSymbols.test(string.slice(1));
+
+const isValidTag = (tag) => startsWithSymbol(tag) && hasValidLength(tag) && hasValidSymbols(tag);
+
+const hasValidCount = (tags, maxCount = 5) => tags.length <= maxCount;
+
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+export function validateHashtags (value) {
+  const tags = value.split(' ');
+  return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
+}
